16533649a375f3e016a4ded337b679090d0d88ee
Tomasz Skorupa <tomasz@adgear.com>
2015-12-14 11:13:41 -0500

    [UPDATE] refactor test suite for clarity

--- test/record_formatter/base_test.rb
+++ test/record_formatter/base_test.rb
@@ -3,52 +3,40 @@ require 'record_formatter/base'
 
 class RecordFormatterBaseTest < ActiveSupport::TestCase
 
-  test "format_records with defaults" do
-    klass = dummy_class
-    klass.expects( RecordFormatter::Base::DEFAULT_SCOPE ).once
-    klass.expects(:select).with( all_of([]) ).once.returns(klass)
+  DummyClass = Class.new(ActiveRecord::Base)
 
-    formatter = RecordFormatter::Base.new klass
+  test "format_records with defaults" do
+    DummyClass.expects( RecordFormatter::Base::DEFAULT_SCOPE ).once
+    DummyClass.expects(:select).with( all_of([]) ).once.returns(DummyClass)
+
+    formatter = RecordFormatter::Base.new DummyClass
     formatter.format_records
   end
 
   test "format_records with plucked columns" do
-    klass = dummy_class
-    klass.expects( RecordFormatter::Base::DEFAULT_SCOPE ).once
-    klass.expects(:select).with( all_of(["id AS id"]) ).once.returns(klass)
-    formatter = RecordFormatter::Base.new klass
+    DummyClass.expects( RecordFormatter::Base::DEFAULT_SCOPE ).once
+    DummyClass.expects(:select).with( all_of(["id AS id"]) ).once.returns(DummyClass)
+    formatter = RecordFormatter::Base.new DummyClass
 
     formatter.columns :id
     formatter.format_records
   end
 
   test "format_records with aliased columns" do
-    klass = dummy_class
-    klass.expects( RecordFormatter::Base::DEFAULT_SCOPE ).once
-    klass.expects(:select).with( all_of(["id AS foo"]) ).once.returns(klass)
-    formatter = RecordFormatter::Base.new klass
+    DummyClass.expects( RecordFormatter::Base::DEFAULT_SCOPE ).once
+    DummyClass.expects(:select).with( all_of(["id AS foo"]) ).once.returns(DummyClass)
+    formatter = RecordFormatter::Base.new DummyClass
 
     formatter.columns :id, alias: :foo
     formatter.format_records
   end
 
   test "format_records with custom scope" do
-    klass = dummy_class
-    klass.expects(:foo).once
-    klass.expects(:select).with( all_of([]) ).once.returns(klass)
+    DummyClass.expects(:foo).once
+    DummyClass.expects(:select).with( all_of([]) ).once.returns(DummyClass)
 
-    formatter = RecordFormatter::Base.new klass, scope: :foo
+    formatter = RecordFormatter::Base.new DummyClass, scope: :foo
     formatter.format_records
-  end
-
-  private
-
-  def dummy_class
-    Class.new(ActiveRecord::Base) do
-      def self.name
-        "Dummy"
-      end
-    end
   end
 
 end
--- test/record_formatter_test.rb
+++ test/record_formatter_test.rb
@@ -3,77 +3,44 @@ require 'record_formatter'
 
 class RecordFormatterTest < ActiveSupport::TestCase
 
+  DummyClass = Class.new
+  DummyClassWithIncludedModule = Class.new(DummyClass) { include RecordFormatter }
+  ActiveRecordSubclass = Class.new(ActiveRecord::Base) { self.abstract_class = true }
+  ARSublassWithActsAs = Class.new(ActiveRecordSubclass) { acts_as_record_formatter }
+  ARSublassWithActsAsAndCustomScope = Class.new(ActiveRecordSubclass) do
+    acts_as_record_formatter custom_method_name: :foobarbaz
+  end
+
   test "dummy class does not define format_records" do
     assert_raises(NoMethodError) do
-      dummy_class.format_records
+      DummyClass.format_records
     end
   end
 
   test "dummy class + included module does not define format_records" do
     assert_raises(NoMethodError) do
-      dummy_class_with_included_module.format_records
+      DummyClassWithIncludedModule.format_records
     end
   end
 
   test "dummy class inheriting from ActiveRecord::Base does not define format_records" do
     assert_raises(NoMethodError) do
-      dummy_class_inheriting_active_record.format_records
+      ActiveRecordSubclass.format_records
     end
   end
 
   test "dummy class inheriting from ActiveRecord::Base + acts_as_record_formatter responds to format_records scope" do
     RecordFormatter::Base.any_instance.expects(:format_records).once.returns('result')
-    dummy_class_with_acts_as.format_records
-    assert_kind_of RecordFormatter::Base, dummy_class_with_acts_as.acts_as_record_formatter
+    ARSublassWithActsAs.format_records
+    assert_kind_of RecordFormatter::Base, ARSublassWithActsAs.acts_as_record_formatter
   end
 
   test "dummy class inheriting from ActiveRecord::Base + acts_as_record_formatter with custom \
       method name responds to foobarbaz scope" do
     RecordFormatter::Base.any_instance.expects(:format_records).once.returns('result')
-    test_subject = dummy_class_with_acts_as(:foobarbaz)
-    test_subject.foobarbaz
-    assert_raises(NoMethodError) do
-      test_subject.format_records
-    end
-    assert_kind_of RecordFormatter::Base, test_subject.acts_as_record_formatter
-  end
-
-  private
-
-  def dummy_class
-    Class.new do
-      def self.name
-        "Dummy"
-      end
-    end
-  end
-
-  def dummy_class_with_included_module
-    Class.new do
-      include RecordFormatter
-      def self.name
-        "DummyWithModule"
-      end
-    end
-  end
-
-  def dummy_class_inheriting_active_record
-    Class.new(ActiveRecord::Base) do
-      self.abstract_class = true
-      def self.name
-        "DummyAR"
-      end
-    end
-  end
-
-  def dummy_class_with_acts_as custom_method_name=nil
-    Class.new(ActiveRecord::Base) do
-      self.abstract_class = true
-      acts_as_record_formatter custom_method_name: custom_method_name
-      def self.name
-        "DummyActsAs"
-      end
-    end
+    ARSublassWithActsAsAndCustomScope.foobarbaz
+    assert_raises(NoMethodError) { ARSublassWithActsAsAndCustomScope.format_records }
+    assert_kind_of RecordFormatter::Base, ARSublassWithActsAsAndCustomScope.acts_as_record_formatter
   end
 
 end
